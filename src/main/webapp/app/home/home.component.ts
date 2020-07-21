22d8b5779017a2692515e90486f9102c457a7c46
Ludovic Scholz <ludovicscholz@gmail.com>
2020-07-22 01:25:51 +0200

    wip

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
-import { KanbanTable } from 'app/shared/model/kanban-table.model';
+import { KanbanTable, IKanbanTable } from 'app/shared/model/kanban-table.model';
 import { KanbanTableService } from 'app/entities/kanban-table/kanban-table.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'jhi-home',
@@ -17,11 +19,18 @@ export class HomeComponent implements OnInit, OnDestroy {
   authSubscription?: Subscription;
   tables: KanbanTable[] | null = [];
   isInputCardShowed = false;
+  isSaving = false;
+
+  editForm = this.fb.group({
+    title: [null, [Validators.required]],
+    desc: [null],
+  });
 
   constructor(
     private accountService: AccountService,
     private loginModalService: LoginModalService,
-    private kanbanTableService: KanbanTableService
+    private kanbanTableService: KanbanTableService,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -47,5 +56,43 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   clickOnCreateBtn(): void {
     this.isInputCardShowed = true;
+  }
+
+  save(): void {
+    this.isSaving = true;
+    const kanbanTable = this.createFromForm();
+    if (kanbanTable.id !== undefined) {
+      this.subscribeToSaveResponse(this.kanbanTableService.update(kanbanTable));
+    } else {
+      this.subscribeToSaveResponse(this.kanbanTableService.create(kanbanTable));
+    }
+  }
+
+  private createFromForm(): IKanbanTable {
+    return {
+      ...new KanbanTable(),
+      id: this.editForm.get(['name'])!.value,
+      description: this.editForm.get(['description'])!.value,
+    };
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IKanbanTable>>): void {
+    result.subscribe(
+      () => this.onSaveSuccess(),
+      () => this.onSaveError()
+    );
+  }
+
+  protected onSaveSuccess(): void {
+    this.isSaving = false;
+    this.previousState();
+  }
+
+  protected onSaveError(): void {
+    this.isSaving = false;
+  }
+
+  previousState(): void {
+    this.isInputCardShowed = false;
   }
 }
